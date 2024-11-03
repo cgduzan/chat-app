935465a0545b92ad37d654b65b4fd7e1f9e0c32f
Chris Duzan <cgduzan@gmail.com>
2024-11-03 13:34:33 -0500

    🧪 Add test endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -154,3 +154,10 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
+
+server.on("request", (req, res) => {
+	if (req.url === "/test") {
+		res.write("Hello World");
+		res.end();
+	}
+});
